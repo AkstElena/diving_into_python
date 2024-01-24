f3fda80febd5b8c5505a82913c15089b70298757
Akst Elena <akstelena@yandex.ru>
2024-01-24 08:57:35 +0300

    seminar3 3

---
+++ seminar/seminar_3/task_3.py
@@ -0,0 +1,23 @@
+"""
+Задание №3
+✔ Создайте вручную кортеж содержащий элементы разных типов.
+✔ Получите из него словарь списков, где:
+ключ — тип элемента,
+значение — список элементов данного типа.
+"""
+
+tuple_obj = 1, 2.1, True, None, 'string', 3, 4, 5, False, 'elem'
+dict_obj = {}
+for item in tuple_obj:
+    item_type = type(item)
+    list_item = []
+    for elem in tuple_obj:
+        if type(elem) == item_type:
+            list_item.append(elem)
+    dict_obj[item_type] = list_item
+print(dict_obj)
+
+dict_obj = {}
+for item in tuple_obj:
+    dict_obj.setdefault(type(item), []).append(item)
+print(dict_obj)
---
+++ seminar/seminar_3/task_5.py
@@ -0,0 +1,22 @@
+"""
+Задание №5
+✔ Создайте вручную список с повторяющимися целыми числами.
+✔ Сформируйте список с порядковыми номерами
+нечётных элементов исходного списка.
+✔ Нумерация начинается с единицы.
+
+"""
+my_list = [1, 2, 1, 3, 3, 5, 7, 8, 9]
+
+new_list = []
+for i in range(len(my_list)):
+    if my_list[i] % 2:
+        new_list.append(i + 1)
+print(new_list)
+
+
+my_list = [i + 1 for i in range(len(my_list)) if my_list[i] % 2]
+print(new_list)
+my_list = [1, 2, 1, 2, 3, 3, 4, 4]
+my_list = [i + 1 for i, el in enumerate(my_list) if el % 2]
+print(my_list)
